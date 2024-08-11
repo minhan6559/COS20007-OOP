de8352795f695ce203b03b23f5368a6586561f14
minhan6559 <minhan6559@gmail.com>
2024-08-11 20:23:07 +1000

    update week 2

---
+++ Week 2/UnitTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTest;
+using MinhAnNguyen;
+
+public class TestMinhAnNguyen
+{
+    [Test]
+    public void Test1()
+    {
+        Student a = new Student();
+        Assert.AreEqual(a.GetName(), "Minh An Nguyen - 104844794");
+    }
+}
