65b85bc3ccb88b33170c7b41bbb1fa8c5e25647f
minhan6559 <minhan6559@gmail.com>
2024-08-26 10:37:09 +1000

    update 5.2

---
+++ Week_5/5.2/SwinAdventure/Player.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    public class Player : GameObject
+    {
+        private Inventory _inventory;
+
+        public Player(string name, string desc) : base(new string[] { "me", "inventory" }, name, desc)
+        {
+            _inventory = new Inventory();
+        }
+
+        public GameObject? Locate(string id)
+        {
+            if (AreYou(id))
+                return this;
+
+            if (_inventory.HasItem(id))
+                return _inventory.Fetch(id);
+
+            return null;
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return $"You are {Name}, {base.FullDescription}\n" +
+                       $"You are carrying:\n{_inventory.ItemList}";
+            }
+        }
+
+        public Inventory Inventory => _inventory;
+    }
+}
